23e8fc678e49b0da9320ea7803c305d517adf72c
Srikanth-gunja <160750302+Srikanth-gunja@users.noreply.github.com>
2024-03-06 21:17:53 +0530

    Update default view to login page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react'
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import Todo from './Components/Todo';
+
 export default function App() {
-  const isAuthenticated=true;
- 
+  const isAuthenticated = true;
 
   return (
     <>
-    <BrowserRouter>
-       <Routes>
-           <Route path='/' element={<Login></Login>} />
-           <Route path='/register' element={<Register></Register>} />
-           <Route path='/Todo' element={isAuthenticated?(<Todo></Todo>):(<Login></Login>)}/>
-           
-           
-           
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/todo"
+            element={isAuthenticated ? <Todo /> : <Navigate to="/login" />}
+          />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   );
 }
+
